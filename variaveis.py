d2f73af32c87d976500f7da798e6d035e3e7e5e7
MILTONRUBRO <miltonrubro@gmail.com>
2017-12-01 14:40:54 -0200

    usando strings e numericos

--- variaveis.py
+++ variaveis.py
@@ -2,3 +2,29 @@
 speed_of_light = 299792458 #metros por segundo
 cycles_per_second = 2700000000.0 #2.t GHZ
 print(speed_of_light/cycles_per_second)
+
+#dias vividos
+idade = 26
+dias_por_ano = 365.25
+
+print(idade*dias_por_ano )
+
+#strings 
+nome = "Ashtar Sheeran"
+print(nome)
+
+#indice na string
+palavra = "palavra"
+print(palavra[0:4])
+
+#imprimir nome com maiuscula
+s = "amilton"
+
+meu_nome = "M" + s[2:]
+
+print(meu_nome)
+
+
+#metodo find
+filosofo = "Penso logo desisto"
+print(filosofo.find("logo", 1))
