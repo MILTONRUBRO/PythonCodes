946d5df547dba23f1eac913b3fbb917e1caea32e
MILTONRUBRO <miltonrubro@gmail.com>
2017-12-02 00:08:37 -0200

    Usando funcoes

---
+++ funcoes.py
@@ -0,0 +1,14 @@
+#funcoes 
+#funcao que retorna uma string sem a primeira letra
+def resto_da_string(s):
+	return s[1:]
+
+#chamando a funcao dentro do print passando um argumento
+print(resto_da_string("Valhalla"))
+
+
+#retorna um numero incremetando 1
+def inc(num):
+	return num+1
+
+print(inc(6))
