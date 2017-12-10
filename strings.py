ed8bfebeb82e942012490cdc712b89951003ab79
MILTONRUBRO <miltonrubro@gmail.com>
2017-12-10 00:31:24 -0200

    usando strings em python

---
+++ strings.py
@@ -0,0 +1,20 @@
+# usando strings 
+#compara duas strings 
+
+def weekend(day):
+    # your code here
+    
+    if(day == 'Saturday' or day == 'Sunday'):
+        return True
+    else:
+        return False
+        
+    
+print weekend('Monday')
+#>>> False
+
+print weekend('Saturday')
+#>>> True
+
+print weekend('July')
+#>>> False
